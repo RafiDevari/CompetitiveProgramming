99b8ca5cfa6161086e068b3a437bb20979a893da
RafiDevari <rafidevari2004@gmail.com>
2024-05-10 08:49:17 +0700

    soal pecahan

---
+++ Pecahan.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    double a,b,c,d,e,f;
+    cin>>a>>b>>c>>d;
+    
+    if (a*d>b*c)
+    {
+        cout<<"lebih besar";
+    }
+    else if (a*d<b*c)
+    {
+        cout<<"lebih kecil";
+    }
+    else if (a*d==b*c)
+    {
+        cout<<"sama";
+    }
+    
+
+
+    return 0;
+
+    
+
+
+
+    
+
+
+    
+
+    
+}
