e55bb1d1cc1ff2a80135924cc4a9f10fde0d708d
RafiDevari <rafidevari2004@gmail.com>
2024-05-13 19:39:28 +0700

    A

--- Pecahan.cpp
+++ Pecahan.cpp
@@ -5,21 +5,12 @@
 using namespace std;
 
 int main(){
-    double a,b,c,d,e,f;
+    double a,b,c,d;
     cin>>a>>b>>c>>d;
     
-    if (a*d>b*c)
-    {
-        cout<<"lebih besar";
-    }
-    else if (a*d<b*c)
-    {
-        cout<<"lebih kecil";
-    }
-    else if (a*d==b*c)
-    {
-        cout<<"sama";
-    }
+    if (a*d>b*c) cout<<"lebih besar";
+    else if (a*d<b*c) cout<<"lebih kecil";
+    else if (a*d==b*c) cout<<"sama";
     
 
 
